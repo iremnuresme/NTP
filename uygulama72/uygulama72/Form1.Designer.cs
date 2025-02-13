f6ee2f0cbb61f656807ad3b7678ae7e665505166
iremnuresme <iremesme5@gmail.com>
2025-02-13 15:45:07 +0300

    Ders Hesaplama Örnek

---
+++ uygulama72/uygulama72/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace uygulama72
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txtDersAdi = new System.Windows.Forms.TextBox();
+            this.txtDersSaati = new System.Windows.Forms.TextBox();
+            this.txtnot1 = new System.Windows.Forms.TextBox();
+            this.txtnot2 = new System.Windows.Forms.TextBox();
+            this.txtperformans1 = new System.Windows.Forms.TextBox();
+            this.txtperformans2 = new System.Windows.Forms.TextBox();
+            this.lblDersAdi = new System.Windows.Forms.Label();
+            this.lblDersSaati = new System.Windows.Forms.Label();
+            this.lblNot1 = new System.Windows.Forms.Label();
+            this.lblNot2 = new System.Windows.Forms.Label();
+            this.lblPerformans1 = new System.Windows.Forms.Label();
+            this.lblPerformans2 = new System.Windows.Forms.Label();
+            this.dgvListe = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.btnSonuc = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtDersAdi
+            // 
+            this.txtDersAdi.Location = new System.Drawing.Point(121, 29);
+            this.txtDersAdi.Name = "txtDersAdi";
+            this.txtDersAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtDersAdi.TabIndex = 0;
+            // 
+            // txtDersSaati
+            // 
+            this.txtDersSaati.Location = new System.Drawing.Point(121, 55);
+            this.txtDersSaati.Name = "txtDersSaati";
+            this.txtDersSaati.Size = new System.Drawing.Size(100, 20);
+            this.txtDersSaati.TabIndex = 1;
+            // 
+            // txtnot1
+            // 
+            this.txtnot1.Location = new System.Drawing.Point(121, 81);
+            this.txtnot1.Name = "txtnot1";
+            this.txtnot1.Size = new System.Drawing.Size(100, 20);
+            this.txtnot1.TabIndex = 2;
+            // 
+            // txtnot2
+            // 
+            this.txtnot2.Location = new System.Drawing.Point(121, 107);
+            this.txtnot2.Name = "txtnot2";
+            this.txtnot2.Size = new System.Drawing.Size(100, 20);
+            this.txtnot2.TabIndex = 3;
+            // 
+            // txtperformans1
+            // 
+            this.txtperformans1.Location = new System.Drawing.Point(121, 133);
+            this.txtperformans1.Name = "txtperformans1";
+            this.txtperformans1.Size = new System.Drawing.Size(100, 20);
+            this.txtperformans1.TabIndex = 4;
+            // 
+            // txtperformans2
+            // 
+            this.txtperformans2.Location = new System.Drawing.Point(121, 159);
+            this.txtperformans2.Name = "txtperformans2";
+            this.txtperformans2.Size = new System.Drawing.Size(100, 20);
+            this.txtperformans2.TabIndex = 5;
+            // 
+            // lblDersAdi
+            // 
+            this.lblDersAdi.AutoSize = true;
+            this.lblDersAdi.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblDersAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDersAdi.Location = new System.Drawing.Point(50, 33);
+            this.lblDersAdi.Name = "lblDersAdi";
+            this.lblDersAdi.Size = new System.Drawing.Size(65, 16);
+            this.lblDersAdi.TabIndex = 7;
+            this.lblDersAdi.Text = "Ders Adı :";
+            // 
+            // lblDersSaati
+            // 
+            this.lblDersSaati.AutoSize = true;
+            this.lblDersSaati.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblDersSaati.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDersSaati.Location = new System.Drawing.Point(39, 56);
+            this.lblDersSaati.Name = "lblDersSaati";
+            this.lblDersSaati.Size = new System.Drawing.Size(76, 16);
+            this.lblDersSaati.TabIndex = 8;
+            this.lblDersSaati.Text = "Ders Saati :";
+            // 
+            // lblNot1
+            // 
+            this.lblNot1.AutoSize = true;
+            this.lblNot1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblNot1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNot1.Location = new System.Drawing.Point(71, 82);
+            this.lblNot1.Name = "lblNot1";
+            this.lblNot1.Size = new System.Drawing.Size(44, 16);
+            this.lblNot1.TabIndex = 9;
+            this.lblNot1.Text = "Not 1 :";
+            // 
+            // lblNot2
+            // 
+            this.lblNot2.AutoSize = true;
+            this.lblNot2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblNot2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNot2.Location = new System.Drawing.Point(71, 109);
+            this.lblNot2.Name = "lblNot2";
+            this.lblNot2.Size = new System.Drawing.Size(44, 16);
+            this.lblNot2.TabIndex = 10;
+            this.lblNot2.Text = "Not 2 :";
+            // 
+            // lblPerformans1
+            // 
+            this.lblPerformans1.AutoSize = true;
+            this.lblPerformans1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPerformans1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPerformans1.Location = new System.Drawing.Point(23, 134);
+            this.lblPerformans1.Name = "lblPerformans1";
+            this.lblPerformans1.Size = new System.Drawing.Size(92, 16);
+            this.lblPerformans1.TabIndex = 11;
+            this.lblPerformans1.Text = "Performans 1 :";
+            // 
+            // lblPerformans2
+            // 
+            this.lblPerformans2.AutoSize = true;
+            this.lblPerformans2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPerformans2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPerformans2.Location = new System.Drawing.Point(23, 160);
+            this.lblPerformans2.Name = "lblPerformans2";
+            this.lblPerformans2.Size = new System.Drawing.Size(92, 16);
+            this.lblPerformans2.TabIndex = 12;
+            this.lblPerformans2.Text = "Performans 2 :";
+            // 
+            // dgvListe
+            // 
+            this.dgvListe.AllowUserToAddRows = false;
+            this.dgvListe.AllowUserToDeleteRows = false;
+            this.dgvListe.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvListe.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvListe.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.dgvListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle11.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle11.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvListe.DefaultCellStyle = dataGridViewCellStyle11;
+            this.dgvListe.Location = new System.Drawing.Point(33, 304);
+            this.dgvListe.Name = "dgvListe";
+            this.dgvListe.ReadOnly = true;
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle12.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvListe.RowHeadersDefaultCellStyle = dataGridViewCellStyle12;
+            this.dgvListe.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvListe.Size = new System.Drawing.Size(793, 257);
+            this.dgvListe.TabIndex = 13;
+            this.dgvListe.SelectionChanged += new System.EventHandler(this.dgvListe_SelectionChanged);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(26, 199);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(109, 42);
+            this.btnEkle.TabIndex = 14;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnYeni.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYeni.Location = new System.Drawing.Point(161, 199);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(107, 42);
+            this.btnYeni.TabIndex = 15;
+            this.btnYeni.Text = " Yeni";
+            this.btnYeni.UseVisualStyleBackColor = false;
+            this.btnYeni.Click += new System.EventHandler(this.btnYeni_Click);
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(435, 116);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(65, 20);
+            this.lblDurum.TabIndex = 16;
+            this.lblDurum.Text = "Durum :";
+            // 
+            // btnSonuc
+            // 
+            this.btnSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSonuc.Location = new System.Drawing.Point(438, 48);
+            this.btnSonuc.Name = "btnSonuc";
+            this.btnSonuc.Size = new System.Drawing.Size(143, 52);
+            this.btnSonuc.TabIndex = 18;
+            this.btnSonuc.Text = "Sonuç Göster ";
+            this.btnSonuc.UseVisualStyleBackColor = true;
+            this.btnSonuc.Click += new System.EventHandler(this.btnSonuc_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnYeni);
+            this.groupBox1.Controls.Add(this.txtDersAdi);
+            this.groupBox1.Controls.Add(this.txtDersSaati);
+            this.groupBox1.Controls.Add(this.txtnot1);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.txtnot2);
+            this.groupBox1.Controls.Add(this.txtperformans1);
+            this.groupBox1.Controls.Add(this.lblPerformans2);
+            this.groupBox1.Controls.Add(this.txtperformans2);
+            this.groupBox1.Controls.Add(this.lblPerformans1);
+            this.groupBox1.Controls.Add(this.lblDersAdi);
+            this.groupBox1.Controls.Add(this.lblNot2);
+            this.groupBox1.Controls.Add(this.lblDersSaati);
+            this.groupBox1.Controls.Add(this.lblNot1);
+            this.groupBox1.Location = new System.Drawing.Point(46, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(283, 264);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(886, 596);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSonuc);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.dgvListe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDersAdi;
+        private System.Windows.Forms.TextBox txtDersSaati;
+        private System.Windows.Forms.TextBox txtnot1;
+        private System.Windows.Forms.TextBox txtnot2;
+        private System.Windows.Forms.TextBox txtperformans1;
+        private System.Windows.Forms.TextBox txtperformans2;
+        private System.Windows.Forms.Label lblDersAdi;
+        private System.Windows.Forms.Label lblDersSaati;
+        private System.Windows.Forms.Label lblNot1;
+        private System.Windows.Forms.Label lblNot2;
+        private System.Windows.Forms.Label lblPerformans1;
+        private System.Windows.Forms.Label lblPerformans2;
+        private System.Windows.Forms.DataGridView dgvListe;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Button btnSonuc;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ uygulama72/uygulama72/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama72
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Ders> DersListesi = new List<Ders>();
+        Ders secilenDers;
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            string DersAdi = txtDersAdi.Text;
+            int DersSaati = Convert.ToInt32(txtDersSaati.Text);
+            double Not1 = Convert.ToDouble(txtnot1.Text);
+            double Not2 = Convert.ToDouble(txtnot2.Text);
+            double performans1 = Convert.ToDouble(txtperformans1.Text);
+            double performans2 = Convert.ToDouble(txtperformans2.Text);
+
+            Ders ders = new Ders(DersAdi, DersSaati, Not1, Not2, performans1, performans2);
+            DersListesi.Add(ders);
+            Listele();
+            btnEkle.Enabled = false;
+
+            dgvListe.DataSource = DersListesi.ToList();
+        }
+
+        private void Temizle()
+        {
+            txtDersAdi.Clear();
+            txtDersSaati.Clear();
+            txtnot1.Clear();
+            txtnot2.Clear();
+            txtperformans1.Clear();
+            txtperformans2.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ders d1 = new Ders("NTP", 10, 70, 60, 80, 100);
+            Ders d2 = new Ders("Robotik Kodlama", 3, 80, 90, 100, 100);
+            Ders d3 = new Ders("Matematik", 5, 40, 50, 40, 50);
+
+            DersListesi.Add(d1);
+            DersListesi.Add(d2);
+            DersListesi.Add(d3);
+
+            Listele();
+
+        }
+
+        private void Listele()
+        {
+            dgvListe.DataSource = null;
+            dgvListe.DataSource = DersListesi.ToList();
+        }
+
+        private void btnYeni_Click(object sender, EventArgs e)
+        {
+            Temizle();
+            btnEkle.Enabled = true;
+        }
+
+        private void dgvListe_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvListe.SelectedRows.Count > 0)
+            {
+                secilenDers = (Ders)dgvListe.SelectedRows[0].DataBoundItem;
+            }
+
+            txtDersAdi.Text = secilenDers.DersAdi;
+            txtDersSaati.Text = secilenDers.DersSaati.ToString();
+            txtnot1.Text = secilenDers.Not1.ToString();
+            txtnot2.Text = secilenDers.Not2.ToString();
+            txtperformans1.Text = secilenDers.Performans1.ToString();
+            txtperformans2.Text = secilenDers.Performans2.ToString();
+
+        }
+
+        private void btnSonuc_Click(object sender, EventArgs e)
+        {
+            lblDurum.Text = SonucYazdir();
+        }
+
+        private string SonucYazdir()
+        {
+            int kalinanDersSayisi = 0;
+            int toplamDersSaati = 0;
+            double agirlikliOrtalamaToplami = 0;
+            double agirlikliOrtalama = 0;
+            string belge = "";
+
+            foreach (Ders d in DersListesi)
+            {
+                toplamDersSaati += d.DersSaati;
+                agirlikliOrtalamaToplami += d.AgirlikliOrtalama;
+
+                agirlikliOrtalama = Math.Round(agirlikliOrtalamaToplami / toplamDersSaati, 2);
+
+                if (d.Durum == "KALDI")
+                {
+                    kalinanDersSayisi++;
+                }
+            }
+
+            if (kalinanDersSayisi == 0)
+            {
+                if (agirlikliOrtalama >= 50 && agirlikliOrtalama < 70)
+                {
+                    belge = "Geçtiniz";
+                }
+                else if (agirlikliOrtalama >= 70 && agirlikliOrtalama < 85)
+                {
+                    belge = "Teşekkür belgesi aldınız.";
+                }
+                else if (agirlikliOrtalama >= 85 && agirlikliOrtalama <= 100)
+                {
+                    belge = "Takdir belgesi aldınız.";
+                }
+                else
+                {
+                    belge = "Hata!";
+                }
+            }
+            else if (kalinanDersSayisi > 3)
+            {
+                belge = "Sınıfta kaldınız :(";
+            }
+            else
+            {
+                belge = "Sorumlu geçtiniz.";
+            }
+
+
+            string sonuc = $"Ağırlıklı ortalamanız: {agirlikliOrtalama}\nKaldığınız ders sayısı: {kalinanDersSayisi}" +
+                $"\n{belge}";
+            return sonuc;
+        }
+    }
+}
