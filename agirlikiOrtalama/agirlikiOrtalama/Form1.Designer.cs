25c8ebf0bab9e34e6f56e85da799eb8453fa00df
iremnuresme <iremesme5@gmail.com>
2025-03-06 11:01:22 +0300

    Add files via upload

---
+++ agirlikiOrtalama/agirlikiOrtalama/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace agirlikiOrtalama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTikla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbDersSayiAdeti = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblAgirlikliOrtalama = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTikla
+            // 
+            this.btnTikla.Location = new System.Drawing.Point(328, 27);
+            this.btnTikla.Name = "btnTikla";
+            this.btnTikla.Size = new System.Drawing.Size(103, 48);
+            this.btnTikla.TabIndex = 0;
+            this.btnTikla.Text = "Tıkla";
+            this.btnTikla.UseVisualStyleBackColor = true;
+            this.btnTikla.Click += new System.EventHandler(this.btnTikla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ders Sayı Adeti";
+            // 
+            // cmbDersSayiAdeti
+            // 
+            this.cmbDersSayiAdeti.FormattingEnabled = true;
+            this.cmbDersSayiAdeti.Location = new System.Drawing.Point(103, 35);
+            this.cmbDersSayiAdeti.Name = "cmbDersSayiAdeti";
+            this.cmbDersSayiAdeti.Size = new System.Drawing.Size(204, 21);
+            this.cmbDersSayiAdeti.TabIndex = 2;
+            this.cmbDersSayiAdeti.SelectedIndexChanged += new System.EventHandler(this.cmbDersSayiAdeti_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(25, 111);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 407);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(231, 111);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(200, 407);
+            this.flowLayoutPanel2.TabIndex = 4;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(437, 111);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(200, 407);
+            this.flowLayoutPanel3.TabIndex = 5;
+            // 
+            // lblAgirlikliOrtalama
+            // 
+            this.lblAgirlikliOrtalama.AutoSize = true;
+            this.lblAgirlikliOrtalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAgirlikliOrtalama.Location = new System.Drawing.Point(643, 122);
+            this.lblAgirlikliOrtalama.Name = "lblAgirlikliOrtalama";
+            this.lblAgirlikliOrtalama.Size = new System.Drawing.Size(41, 13);
+            this.lblAgirlikliOrtalama.TabIndex = 6;
+            this.lblAgirlikliOrtalama.Text = "label2";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(643, 198);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(41, 13);
+            this.lblDurum.TabIndex = 0;
+            this.lblDurum.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(88, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ders Adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(292, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ders Saati";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(484, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ders Notu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(830, 539);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblAgirlikliOrtalama);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.cmbDersSayiAdeti);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTikla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTikla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbDersSayiAdeti;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label lblAgirlikliOrtalama;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ agirlikiOrtalama/agirlikiOrtalama/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace agirlikiOrtalama
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+      
+
+
+    
+
+        private void btnTikla_Click(object sender, EventArgs e)
+        {
+            double toplamDers = 0;
+            double toplamAgirlik = 0;
+            double agirlikliOrtalama = 0;
+            int basarisizDersSayisi = 0;
+
+            for (int i = 0; i < flowLayoutPanel2.Controls.Count; i++)
+            {
+                TextBox txtDers = (TextBox)flowLayoutPanel2.Controls[i];
+                TextBox txtOrtalama = (TextBox)flowLayoutPanel3.Controls[i];
+
+                double dersSaati = Convert.ToDouble(txtDers.Text);
+                double dersOrt = Convert.ToDouble(txtOrtalama.Text);
+
+                toplamAgirlik += dersSaati * dersOrt;
+                toplamDers += dersSaati;
+
+                if (dersOrt < 50)
+                {
+                    basarisizDersSayisi++;
+                }
+            }
+
+            // Ağırlıklı Ortalama Hesaplama
+            if (toplamDers > 0)
+            {
+                agirlikliOrtalama = toplamAgirlik / toplamDers;
+            }
+
+            // Durum Mesajını Belirleme
+            if (basarisizDersSayisi == 0)
+            {
+                if (agirlikliOrtalama >= 85)
+                {
+                    lblDurum.Text = "Takdir Aldınız:)";
+                }
+                else if (agirlikliOrtalama >= 70 && agirlikliOrtalama < 85)
+                {
+                    lblDurum.Text = "Teşekkür Aldınız:)";
+                }
+                else
+                {
+                    lblDurum.Text = "Düz Geçtiniz:|";
+                }
+            }
+            else if (basarisizDersSayisi == 3)
+            {
+                lblDurum.Text = "Sorumlu geçtiniz.";
+            }
+            else if (basarisizDersSayisi > 3)
+            {
+                lblDurum.Text = "Sınıfta Kaldınız.";
+            }
+            else
+            {
+                lblDurum.Text = $"{basarisizDersSayisi} adet başarısız dersiniz var.";
+            }
+
+            lblAgirlikliOrtalama.Text = "Ağırlıklı Ortalamanız: " + Math.Round(agirlikliOrtalama, 2);
+        } 
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Takdir Teşekkür Hesaplama Programı";
+            this.BackColor = Color.FromArgb(84, 160, 255);
+
+            for (int i = 1; i <= 20; i++)
+            {
+                cmbDersSayiAdeti.Items.Add(i.ToString());
+            }
+        }
+
+        private void cmbDersSayiAdeti_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel2.Controls.Clear();
+            flowLayoutPanel3.Controls.Clear();
+            int adet = Convert.ToInt32(cmbDersSayiAdeti.Text);
+
+            for (int i = 1; i <= adet; i++)
+            {
+                TextBox txt = new TextBox();
+                flowLayoutPanel1.Controls.Add(txt);
+                txt.Text = "Ders " + i.ToString();
+                txt.Width = 200;
+            }
+
+            for (int i = 1; i <= adet; i++)
+            {
+                TextBox txt = new TextBox();
+                //Label lbl = new Label();
+
+                //flowLayoutPanel2.Controls.Add(lbl);
+                flowLayoutPanel2.Controls.Add(txt);
+
+
+                //lbl.Text = "Ders Saati";
+                txt.Width = 200;
+                //lbl.Width = 90;
+            }
+            for (int i = 1; i <= adet; i++)
+            {
+                TextBox txt = new TextBox();
+                //Label lbl = new Label();
+
+                //flowLayoutPanel3.Controls.Add(lbl);
+                flowLayoutPanel3.Controls.Add(txt);
+
+
+                //lbl.Text = "Ortalama";
+                txt.Width = 200;
+                //lbl.Width = 90;
+            }
+
+        }
+    }
+}
+
+
